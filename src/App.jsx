6cae3ee74073224967ab2b40a47365e1d75a0d94
jorgeduje <jorgeduje4@gmail.com>
2023-09-20 11:58:40 -0300

    se trabaja con componentes props e iconos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+// importacion por defecto
+// import EsteEsElHome from "./components/pages/home/Home"
+
+import { Navbar } from "./components/layout/navbar/Navbar";
+import { Home } from "./components/pages/home/Home";
+import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
+import { Login } from "./components/pages/login/Login";
+
 function App() {
+  
+  let saludo = "Hola pepito como estas"
+
   return (
     <div>
-      <h1>App</h1>
+      <Navbar />
+      {/* <Home />
+      <Login /> */}
+      <ItemListContainer saludo={saludo} edad={12} />
     </div>
   );
 }
 
 export default App;
+
+
+// const sumar = (a, b)=>{
+//   return a + b
+// }
+
+// <Sumar a={12} b={1}/>
---
+++ src/components/common/cartWidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+import {BsFillCartPlusFill} from "react-icons/bs"
+
+const CartWidget = () => {
+  return (
+    <div>
+        <span>2</span>
+        <BsFillCartPlusFill />
+    </div>
+  )
+}
+
+export default CartWidget
