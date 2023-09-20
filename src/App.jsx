f7cedf857a492ff6058df55d34a81aada3c6bcac
jorgeduje <jorgeduje4@gmail.com>
2023-09-20 09:44:53 -0300

    limpiando main

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,9 @@
-import { Footer } from "./Footer";
-import { Home } from "./Home";
-import { Navbar } from "./Navbar";
-
 function App() {
-  let nombre = "pepito";
-
-  const sumar = () => {
-    console.log("estoy sumar");
-  };
-
-  // let estilos = {
-  //   color: "red"
-  // }
-
   return (
     <div>
-      <Navbar />
-
-      {/* el comentario */}
-      <Home />
-
-      <Footer />
+      <h1>App</h1>
     </div>
   );
 }
 
 export default App;
-
-// App() / <App /> ---> <div>
-// <h1 style={{ color: "red", fontSize: "2rem" }} >{nombre}</h1>
-// <button onClick={sumar}>sumar</button>
-// </div>
-
-// const saludar = ()=>{
-
-//   return "hola como estas"
-// }
-
-// saludar() --> "hola como estas"
