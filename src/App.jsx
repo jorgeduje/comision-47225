82b06ac82580705c73834186432b069bbb6119b6
jorgeduje <jorgeduje4@gmail.com>
2023-10-05 09:13:48 -0300

    se ordena la rama main

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import CounterContainer from "./components/common/counter/CounterContainer";
-import CustomModal from "./components/common/modal/CustomModal";
-import { FetchDos } from "./components/fetching/FetchDos";
-import { FetchUno } from "./components/fetching/FetchUno";
 import { Navbar } from "./components/layout/navbar/Navbar";
-import ItemDetailContainer from "./components/pages/itemDetailContainer/ItemDetailContainer";
 import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <ItemListContainer /> */}
-      {/* <ItemDetailContainer /> */}
-      {/* <FetchDos /> */}
-      {/* <FetchUno /> */}
-
-      <CustomModal> 
-        <CounterContainer />
-      </CustomModal>
-
-      <CustomModal>
-        <ItemListContainer />
-      </CustomModal>
-   
+      <ItemListContainer />
     </>
   );
 }
