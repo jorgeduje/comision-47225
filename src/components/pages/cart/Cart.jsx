ed1e3b657412e89774a01affa37523a4709ec6ca
jorgeduje <jorgeduje4@gmail.com>
2023-10-11 11:57:54 -0300

    se trabaja con eventos del formulario

--- src/components/pages/cart/Cart.jsx
+++ src/components/pages/cart/Cart.jsx
@@ -1,11 +1,15 @@
-
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   return (
     <div>
-        <h1>Estoy en el carrito</h1>
+      <h1>Estoy en el carrito</h1>
+      <Link to="/checkout">
+        <Button variant="contained">Finalizar compra</Button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "../components/layout/Layout";
+import { routes } from "./menuRoutes";
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        {routes.map(({ id, path, Element }) => {
+          return <Route key={id} path={path} element={<Element />} />;
+        })}
+      </Route>
+
+      <Route path="*" element={<h1>Not found</h1>} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
