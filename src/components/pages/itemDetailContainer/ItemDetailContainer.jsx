cce423858a58c20bd1efc361a988b7a76fdee01f
jorgeduje <jorgeduje4@gmail.com>
2023-10-30 09:48:13 -0300

    se corrige el bug para obtener las cantidades de un producto que hay en el cart

--- src/components/pages/itemDetailContainer/ItemDetailContainer.jsx
+++ src/components/pages/itemDetailContainer/ItemDetailContainer.jsx
@@ -4,11 +4,10 @@ import { ItemDetail } from "./ItemDetail";
 import { useParams } from "react-router-dom";
 import { CartContext } from "../../../context/CartContext";
 
-
 import Swal from "sweetalert2";
 import { db } from "../../../firebaseConfig";
 
-import { getDoc, collection, doc } from "firebase/firestore"
+import { getDoc, collection, doc } from "firebase/firestore";
 
 const ItemDetailContainer = () => {
   const [productSelected, setProductSelected] = useState({});
@@ -18,21 +17,17 @@ const ItemDetailContainer = () => {
 
   const { addToCart, getQuantityById } = useContext(CartContext);
 
-  let totalQuantity = getQuantityById(+id);
-  console.log(totalQuantity);
+  let totalQuantity = getQuantityById(id);
+
 
   useEffect(() => {
+    let itemCollection = collection(db, "products");
 
-    let itemCollection = collection(db, "products")
+    let refDoc = doc(itemCollection, id);
 
-    let refDoc = doc(itemCollection, id )
-
-    getDoc(refDoc).then((res)=>{
-      setProductSelected( {id: res.id, ...res.data()})
-    })
-
-
-   
+    getDoc(refDoc).then((res) => {
+      setProductSelected({ id: res.id, ...res.data() });
+    });
   }, [id]);
 
   const onAdd = (cantidad) => {
